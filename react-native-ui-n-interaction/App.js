bfde2f586b919c578f0f62f839820421102e59c1
elgeeee <ellowlayn@gmail.com>
2026-02-13 15:57:28 +0800

    Done with fixed dimensions section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import LotsOfStyles from './components/Style.js';
+// import LotsOfStyles from './components/Style.js';
+import FixedDimensionsBasics from './components/FixedDimensions.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LotsOfStyles />
+      {/* <LotsOfStyles /> */}
+      <FixedDimensionsBasics />
+      <StatusBar />
     </View>
   );
 }
