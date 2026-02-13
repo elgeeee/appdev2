6436b28d9738f63987b93468c38080659064e1b4
elgeeee <ellowlayn@gmail.com>
2026-02-13 16:34:23 +0800

    Done with flex direction section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native';
-import Flex from './components/Flex.js';
+// import Flex from './components/Flex.js';
+import FlexDirectionBasics from './components/FlexDirection.js';
 
 export default function App() {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Flex />
+      {/* <Flex /> */}
+      <FlexDirectionBasics />
       <StatusBar />
     </SafeAreaView>
   );
